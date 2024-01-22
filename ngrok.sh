83f37285a1e74fec5308004d43b838398f1ec6a8
123itsfree <131263381+123itsfree@users.noreply.github.com>
2024-01-22 18:08:43 +0600

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,5 @@
 # FOR x86 servers using linux : 
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok-v3-stable-linux-amd64.tgz
-./ngrok config add-authtoken 2XWvdIQegnz7fRUXkMvJWVy7Z5p_3SijirFvadrRgBJot32fG #Put Yours here
+./ngrok config add-authtoken 2YWsaVThSodEO6ezjfYxqm0UUEo_2MQ2tN1YgY5bbgFcuHadx #Put Yours here
 ./ngrok http 8080
